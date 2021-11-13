81b01ccb00f29c3afb01e0df8fd92c18ce09d8df
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-11-13 13:12:40 -0800

    initailized react and setup react env

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(express.json({ extended: false }));
 app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/profile", require("./routes/profile"));
 
 // app.get('/', (req, res) => res.send({ message: 'Hello to the taskBucket' }))
 
